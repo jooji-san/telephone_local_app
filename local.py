9f07f3687848a0a507a58df74a3eb8241f74088b
jooji-san <g.shengelaia@gmail.com>
2023-06-01 18:11:42 +0400

    rename main to local

--- local.py
+++ local.py
@@ -23,19 +23,32 @@ def connect_wifi():
     with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'w') as f:
         f.writelines(lines)
 
-    #run('sudo rfkill unblock wifi', shell=True)
-    #run('sudo wpa_cli -i wlan0 reconfigure')
+    run('sudo rfkill unblock wifi', shell=True)
+    run('sudo wpa_cli -i wlan0 reconfigure')
     
-def get_config_json():
-    with open('../config.json', 'r') as f:
+def get_config_json(isAuth=False):
+    path = '../config.json'
+    if isAuth:
+        path = '../auth.json'
+
+    with open(path, 'r') as f:
         json_lines = f.read()
         config = json.loads(json_lines)
         print(config)
         return config
+    
+def write_config_json(dic, isAuth=False):
+    path = '../config.json'
+    if isAuth:
+        path = '../auth.json'
+
+    with open(path, 'w') as f:
+        configJSON = json.dumps(dic)
+        f.write(configJSON)
 
 def is_connected():
     try:
-        response = urlopen('https://www.google.com/', timeout=10)
+        urlopen('https://www.google.com/', timeout=10)
         return True
     except: 
         return False
